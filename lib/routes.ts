d307bf21369cafa2add864a5bc3cfa79b77b38a7
paolaklwlqm <pao.lobato200614@gmail.com>
2025-11-10 17:35:30 -0600

    carrera

--- lib/routes.ts
+++ lib/routes.ts
@@ -1,15 +1,19 @@
 import { AreaRoutes } from "./modules/area/routes/area.routes";
 import { UsuarioRoutes } from "./modules/usuario/routes/usuario.routes";
 import { CicloEscolarRoutes } from "./modules/cicloEscolar/routes/cicloEscolar.routes";
+import { CarreraRoutes } from "./modules/carrera/routes/carrera.routers";
+
 export class Routes {
 
     private usuarioRoutes: UsuarioRoutes = new UsuarioRoutes();
     private areaRoutes: AreaRoutes = new AreaRoutes();
     private cicloEscolarRoutes: CicloEscolarRoutes = new CicloEscolarRoutes();
+    private carreraRoutes: CarreraRoutes = new CarreraRoutes();
 
     public routes(app): void {
         this.usuarioRoutes.routes(app);
         this.areaRoutes.routes(app);
         this.cicloEscolarRoutes.routes(app);
+        this.carreraRoutes.routes(app);
     }
 }
